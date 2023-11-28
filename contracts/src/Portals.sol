1dcddb36fe8891478c49c687a3eb1c32bad8e0ea
Vedant Chainani <vedantchainani1084@gmail.com>
2023-11-28 16:17:59 +0530

    add withdraw contracts

--- contracts/src/Portals.sol
+++ contracts/src/Portals.sol
@@ -1,10 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+// ERC-721 Imports
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+
+// CCIP Imports 
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Withdraw} from "./utils/Withdraw.sol";
+
 
 // Avalanche Fuji Router Address - 0x554472a2720e5e7d5d3c817529aba05eed5f82d8
 // Mumbai Tesnet Router Address - 0x70499c328e1e2a3c41108bd3730f6670a44595d1
